2f335e6e7b93e852a83bce1a763851b6c6eb68c7
pineda.juan92 <pineda.juan92@gmail.com>
2021-10-12 21:17:19 -0500

    Saludame1 with greetings for desconocidos

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const app = express();
 
 app.get("/", (req, res) => {
   const { nombre } = req.query;
-  res.send(`<h1>Hola ${nombre}!</h1>`);
+  if(!nombre) res.send(`<h1>Hola desconocido!</h1>`);
+  else {
+      res.send(`<h1>Hola ${nombre}!</h1>`);
+  }
 });
 
 app.listen(3000, () => console.log("Listening on port 3000!"));
