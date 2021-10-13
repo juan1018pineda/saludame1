3d8b17c2470d38895556049a5b40e656e1a760a9
pineda.juan92 <pineda.juan92@gmail.com>
2021-10-12 21:11:11 -0500

    Saludame 1 solution

---
+++ app.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  const { nombre } = req.query;
+  res.send(`<h1>Hola ${nombre}</h1>`);
+});
+
+app.listen(3000, () => console.log("Listening on port 3000!"));
