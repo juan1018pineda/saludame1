19b071abab020d911ab613f3a38d7d8c74797709
pineda.juan92 <pineda.juan92@gmail.com>
2021-10-12 21:15:07 -0500

    Saludame1 with enhancements

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.get("/", (req, res) => {
   const { nombre } = req.query;
-  res.send(`<h1>Hola ${nombre}</h1>`);
+  res.send(`<h1>Hola ${nombre}!</h1>`);
 });
 
 app.listen(3000, () => console.log("Listening on port 3000!"));
